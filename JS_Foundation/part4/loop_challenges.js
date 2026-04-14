0468fa958748bf7568378b976126d60fe702624b
Divy Barot <divybarot15@gmail.com>
2026-04-14 09:14:18 +0530

    Started loop Challenges

---
+++ JS_Foundation/part4/loop_challenges.js
@@ -0,0 +1,13 @@
+/* 
+1. Write a loop that calculates the sum of all the numbers 
+    from 1 to 5 and stores the result in a variable named 'sum'
+*/
+
+let sum = 0;
+let i = 0;
+
+while (i <= 5 ) {
+    sum+=i;
+    i++;
+}
+console.log("1. Addition of first five numbers is : " + sum);
